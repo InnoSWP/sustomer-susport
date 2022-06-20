7a4aa08c02ccd4d34a6d37034ca1023b4e06e20d
Poseidondon <superzarub@rambler.ru>
2022-06-20 18:43:25 +0300

    nlp_router structure established

--- nlp_router.py
+++ nlp_router.py
@@ -1,13 +1,19 @@
 import uvicorn
+from starlette.status import HTTP_201_CREATED, HTTP_404_NOT_FOUND
 from firestore_database import FirestoreDatabase
 from question_entry import QuestionEntry
-from fastapi import FastAPI
-from nlp import susSimProvider
+from fastapi import FastAPI, Response
+from pydantic import BaseModel
 
 SIM_CONST = 0.75
 app = FastAPI()
 cached_questions: list[QuestionEntry] = []
 fd = FirestoreDatabase("firebase/sustomer-susport-private-key.json")
+
+
+class QuestionItem(BaseModel):
+    question: str
+    answer: str
 
 
 @app.on_event("startup")
@@ -16,21 +22,32 @@ def init():
     cached_questions = fd.questions()
 
 
-@app.get("/")
-def root():
-    global cached_questions
-    for q in cached_questions:
-        print(f"{q.question}: {q.answer}")
-    return {"message": "Hello World"}
+@app.get("/", status_code=404)
+def index():
+    return Response(status_code=HTTP_404_NOT_FOUND)
 
 
 @app.get("/similar")
 def similar_questions(question: str, index: float = SIM_CONST):
     global cached_questions
-    for question in cached_questions:
-        susSimProvider.encode_question(str)
+    # TODO: search for similar question
+    for q in cached_questions:
+        print(f"{q.question}: {q.answer}")
 
-    return {"question": question}
+    return {question: str(index)}
+
+
+@app.post("/new", status_code=201)
+def new_question(q_item: QuestionItem):
+    global cached_questions, fd
+    # TODO: get key by question
+    q_key = "new_key"
+    q_entry = QuestionEntry(q_key, q_item.question, q_item.answer)
+
+    fd.set_question(q_entry)
+    cached_questions = fd.questions()
+
+    return Response(status_code=HTTP_201_CREATED)
 
 
 def run_router():
