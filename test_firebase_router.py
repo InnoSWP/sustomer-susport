3d5082c6dc2520bb378def5f416d89e3f6dbc8d1
moflotas <tima.sedov.2004@mail.ru>
2022-06-27 20:55:00 +0300

    added tests to firebase_router

--- test_firebase_router.py
+++ test_firebase_router.py
@@ -13,3 +13,33 @@ def test_teams():
     print()
     for team in teams:
         print(f"{team['team_name']}: {team['members']}, {team['tg_group_id']}")
+
+
+def test_team_funcs():
+    initial_teams = client.get("/teams")
+
+    team = {"team_name": "sustem inc", "members": ['Boris', 'Timofei', 'Stepan', 'Nikita'], "tg_group_id": 'idkwhichnumber'}
+
+    # Add new team to firestore
+    response = client.post("/team", json=team)
+    assert response.status_code == 201
+
+    assert client.get("/teams").json() != initial_teams.json()
+
+    # Get that team
+    response = client.get("/team", params={"team_name": "sustem inc"})
+    assert response.status_code == 200
+
+    # Non-existing team
+    response = client.get("/team", params={"team_name": "Non-existing team&333"})
+    assert response.status_code == 404
+
+    # Delete that team
+    response = client.delete("/delete-team", params={"team_name": "sustem inc"})
+    assert response.status_code == 200
+
+    # Non-existing team
+    response = client.delete("/delete-team", params={"team_name": "Non-existing team&333"})
+    assert response.status_code == 404
+
+    assert client.get("/teams").json() == initial_teams.json()
