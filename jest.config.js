2d49873c20d5bbc487be23bf2c3d6f3380db9efb
Demezy <work.mail.pub@gmail.com>
2022-06-29 10:38:31 +0300

    fix: setup testing lib correctly

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,26 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-	globals: {
-		"ts-jest": {
-			tsconfig: true,
-		}
-	}
+	// fix weird imports in jest
+  transform: {
+    "\\.[jt]sx?$": "ts-jest",
+  },
+  globals: {
+    "ts-jest": {
+      useESM: true,
+    },
+  },
+  moduleNameMapper: {
+    "(.+)\\.js": "$1",
+  },
+  extensionsToTreatAsEsm: [".ts"],
+	// end of fix
+
+	// my config
+  preset: "ts-jest",
+  testEnvironment: "node",
+  globals: {
+    "ts-jest": {
+      tsconfig: true,
+    },
+  },
 };
