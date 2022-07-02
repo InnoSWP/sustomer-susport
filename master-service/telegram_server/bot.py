692e82dc3422e6c7babb99bec175482950ad58d1
tsepanx <77626319+tsepanx@users.noreply.github.com>
2022-07-02 15:39:49 +0300

    move funcs to utils.py & code cleanup

--- master-service/telegram_server/bot.py
+++ master-service/telegram_server/bot.py
@@ -1,101 +1,16 @@
-import enum
 import json
 import logging
 import threading
-from dataclasses import dataclass
-from enum import IntEnum
-from typing import TypeVar, Union
+from typing import Optional
 
 import dotenv
 import telegram
+from telegram.ext import MessageHandler
 from telegram.ext import MessageHandler, Updater, filters, CallbackQueryHandler
 
-from .utils import get_button_markup
 
-
-class IssueState(enum.Enum):
-    open = 'OPEN'
-    progress = 'IN PROGRESS'
-    closed = 'CLOSED'
-
-    def __str__(self):
-        return self.value.__str__()
-
-
-class CallbackQueryType(IntEnum):
-    ASSIGN = 1
-    CLOSE = 2
-    REJECT = 3
-
-    def __str__(self):
-        return self.value.__str__()
-
-
-@dataclass
-class DialogEntity:
-    issue_id: int
-    client_id: int
-
-    volunteer_chat_id: Union[int, None] = None
-
-    question_text: str = ''
-    answer_text: str = ''
-
-    issue_message_id: Union[int, None] = None
-    state: IssueState = IssueState.open
-
-
-def get_issue_message_text(dialog: DialogEntity, user_name=None):
-    issue_line = f'*Issue #{dialog.issue_id}*\n\n'
-    question_line = f'Question: _"{dialog.question_text}"_\n\n'
-
-    if dialog.state == IssueState.open:
-        return f'{issue_line}' \
-               f'{question_line}' \
-               f'status: *OPEN*'
-    elif dialog.state == IssueState.progress and dialog.volunteer_chat_id:
-        return f'{issue_line}' \
-               f'{question_line}' \
-               f'status: *IN PROGRESS* by {user_name}'
-    elif dialog.state == IssueState.closed:
-        return f'{issue_line}' \
-               f'{question_line}' \
-               f'Answers:\n{dialog.answer_text}\n\n' \
-               f'status: *CLOSED* by {user_name}'
-    else:
-        return f'LOL why does this happen?'
-
-
-T = TypeVar('T')
-
-
-def search_by(entities_list: [T], field, value) -> Union[T, None]:
-    l = list(filter(
-        lambda d: d.__getattribute__(field) == value,
-        entities_list
-    ))
-
-    if len(l) >= 1:  # TODO manage it
-        return l[-1]
-    else:
-        return None
-
-
-def keyboard_from_dialog(title: str, dialog: DialogEntity, btn_type: CallbackQueryType, group_message_id):
-    btn_data = json.dumps({
-        'issue_id': dialog.issue_id,
-        'btn_type': btn_type,
-        'group_message_id': group_message_id
-    })
-    return get_button_markup([title, btn_data])
-
-
-def prepare_for_markdown_mode(message: str):
-    message = message.replace('#', '\#')
-    message = message.replace('-', '\-')
-    message = message.replace('.', '\.')
-
-    return message
+from .utils import DialogEntity, IssueState, prepare_for_markdown_mode, \
+    search_by, get_issue_message_text, keyboard_from_dialog, CallbackQueryType
 
 
 class BotThread:
@@ -190,7 +105,7 @@ class BotThread:
 
         print(self.dialogs, issue_id, btn_type)
 
-        d: Union[DialogEntity, None] = search_by(self.dialogs, 'issue_id', issue_id)
+        d: Optional[DialogEntity] = search_by(self.dialogs, 'issue_id', issue_id)
         user_chat_id = update.callback_query.from_user.id
         message_id = update.effective_message.message_id
         user_name = update.callback_query.from_user.name
@@ -227,7 +142,7 @@ class BotThread:
                 reply_markup=keyboard_from_dialog('Mark as completed', d, CallbackQueryType.CLOSE, message_id)
             )
         elif int(btn_type) == CallbackQueryType.CLOSE:
-            if d.state == IssueState.closed:
+            if d.state != IssueState.progress:
                 return
 
             d.state = IssueState.closed
--- master-service/telegram_server/utils.py
+++ master-service/telegram_server/utils.py
@@ -1,5 +1,9 @@
+import enum
+import json
 import logging
+from dataclasses import dataclass
 from functools import wraps
+from typing import Optional, TypeVar
 
 import telegram
 
@@ -26,3 +30,88 @@ def get_button_markup(*buttons: ([str, str])):
     x = lambda t: telegram.InlineKeyboardButton(t[0], callback_data=t[1])
 
     return telegram.InlineKeyboardMarkup.from_row(list(map(x, buttons)))
+
+
+class IssueState(enum.Enum):
+    open = 'OPEN'
+    progress = 'IN PROGRESS'
+    closed = 'CLOSED'
+
+    def __str__(self):
+        return self.value.__str__()
+
+
+class CallbackQueryType(enum.IntEnum):
+    ASSIGN = 1
+    CLOSE = 2
+    REJECT = 3
+
+    def __str__(self):
+        return self.value.__str__()
+
+
+@dataclass
+class DialogEntity:
+    issue_id: int
+    client_id: int
+
+    volunteer_chat_id: Optional[int] = None
+
+    question_text: str = ''
+    answer_text: str = ''
+
+    issue_message_id: Optional[int] = None
+    state: IssueState = IssueState.open
+
+
+def get_issue_message_text(dialog: DialogEntity, user_name=None):
+    issue_line = f'*Issue #{dialog.issue_id}*\n\n'
+    question_line = f'Question: _"{dialog.question_text}"_\n\n'
+
+    if dialog.state == IssueState.open:
+        return f'{issue_line}' \
+               f'{question_line}' \
+               f'status: *OPEN*'
+    elif dialog.state == IssueState.progress and dialog.volunteer_chat_id:
+        return f'{issue_line}' \
+               f'{question_line}' \
+               f'status: *IN PROGRESS* by {user_name}'
+    elif dialog.state == IssueState.closed:
+        return f'{issue_line}' \
+               f'{question_line}' \
+               f'Answers:\n{dialog.answer_text}\n\n' \
+               f'status: *CLOSED* by {user_name}'
+    else:
+        return f'LOL why does this happen?'
+
+
+T = TypeVar('T')
+
+
+def search_by(entities_list: [T], field, value) -> Optional[list[T]]:
+    l = list(filter(
+        lambda d: d.__getattribute__(field) == value,
+        entities_list
+    ))
+
+    if len(l) >= 1:  # TODO manage it
+        return l[-1]
+    else:
+        return None
+
+
+def keyboard_from_dialog(title: str, dialog: DialogEntity, btn_type: CallbackQueryType, group_message_id):
+    btn_data = json.dumps({
+        'issue_id': dialog.issue_id,
+        'btn_type': btn_type,
+        'group_message_id': group_message_id
+    })
+    return get_button_markup([title, btn_data])
+
+
+def prepare_for_markdown_mode(message: str):
+    message = message.replace('#', '\#')
+    message = message.replace('-', '\-')
+    message = message.replace('.', '\.')
+
+    return message
